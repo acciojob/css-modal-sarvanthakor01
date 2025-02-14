f7864d9a8e43f92d32bae2f0e19e117021ac9629
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-14 23:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-//your JS code here. If required.
 document.getElementById("openModal").addEventListener("click", function () {
-    document.getElementById("modal").style.display = "flex";
+    document.getElementById("modal").classList.add("show");
 });
 
 document.querySelector(".close-modal").addEventListener("click", function () {
-    document.getElementById("modal").style.display = "none";
+    document.getElementById("modal").classList.remove("show");
 });
 
-// Close modal when clicking outside of it
-window.addEventListener("click", function (event) {
-    let modal = document.getElementById("modal");
-    if (event.target === modal) {
-        modal.style.display = "none";
+document.getElementById("modal").addEventListener("click", function (event) {
+    if (event.target === this) {
+        this.classList.remove("show");
     }
 });
